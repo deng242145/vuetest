1fc52f6e98345226ff348a8166c3e9e1490304d9
邓伟征 <9776447+deng-weizheng@user.noreply.gitee.com>
2021-11-06 10:16:05 +0800

    完成了登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <el-button type="info" @click="logout">退出</el-button>
+    2222
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout() {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
